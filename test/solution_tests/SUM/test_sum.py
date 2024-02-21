7c93e2e034be23b7f5e361cf4794c51a932de586
sbx_user1051 <sbx_user1051@169.254.33.29>
2024-02-21 16:32:55 +0000

    Wed Feb 21 16:32:55 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -5,3 +5,17 @@ class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
 
+    def test_sum_out_of_range(self):
+        try:
+            sum_solution.compute(101, 2)
+        except Exception as e:
+            assert e.__class__.__name__ == "ValueError"
+            assert str(e) == "x should be between 0 and 100"
+
+    def test_sum_not_int(self):
+        try:
+            sum_solution.compute("a", 2)
+        except Exception as e:
+            assert e.__class__.__name__ == "ValueError"
+            assert str(e) == "x and y should be integers"
+
