cb4788868222bdf5858fc21ca69f56eddfea3043
sbx_user1051 <sbx_user1051@169.254.33.29>
2024-02-21 16:45:05 +0000

    Wed Feb 21 16:45:05 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,10 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
+def checkout(skus: str):
+    # Get the list of items with a number per item
+    items = {}
+    items_list = list(skus)
+    
     raise NotImplementedError()
+
