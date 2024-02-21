0e5f7420305b1e3648ccf7fc03c4d00d4ffb0db6
sbx_user1051 <sbx_user1051@169.254.33.29>
2024-02-21 18:33:31 +0000

    Wed Feb 21 18:33:31 UTC 2024

--- lib/solutions/CHK/offer_helper_functions.py
+++ lib/solutions/CHK/offer_helper_functions.py
@@ -1,17 +1,18 @@
 def n_for_m_offer(items, sku, sku_count, offer):
     # Check that offer is possible
-    if sku_count < offer.count:
+    if sku_count < offer["count"]:
         return 0
-    value = (sku_count // offer.count) * offer.price
-    items[sku] -= offer.count * (sku_count // offer.count)
+    value = (sku_count // offer["count"]) * offer["price"]
+    items[sku] -= offer["count"] * (sku_count // offer["count"])
     return value
 
 
 def x_for_y_offer(items, sku, sku_count, offer):
     # Check if there is enough x for the offer and there is enough y to offer
-    if sku_count < offer.count or items[offer.offer_sku] < offer.offer_count:
+    if sku_count < offer["count"] or items[offer["offer_sku"]] < offer["offer_count"]:
         return 0
     # Remove the free items from the basket
-    items[offer.offer_sku] -= offer.offer_count
+    items[offer["offer_sku"]] -= offer["offer_count"]
     # X items will be charged individually at the end
     return 0
+
