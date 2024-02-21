2e314c1fe0d43a9b2456d41f9efbf6896dc000b9
sbx_user1051 <sbx_user1051@169.254.33.29>
2024-02-21 16:48:05 +0000

    Wed Feb 21 16:48:05 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,43 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str):
+    prices = {
+        'A': {
+            "price": 50,
+            "offers": {
+                "3": 130
+            }
+        },
+        'B': {
+            "price": 30,
+            "offers": {
+                "2": 45
+            }
+        },
+        'C': {
+            "price": 20,
+            "offers": {}
+        },
+        'D': {
+            "price": 15,
+            "offers": {}
+        }
+    }
     # Get the list of items with a number per item
     items = {}
     items_list = list(skus)
-    
+    for item in items_list:
+        if item in items:
+            items[item] += 1
+        else:
+            items[item] = 1
+
+    basket_total = 0
+
+    for item in items:
+        if item not in prices:
+            return -1
+        
     raise NotImplementedError()
+
 
