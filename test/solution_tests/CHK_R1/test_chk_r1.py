c5df1e819e5be22807e8fcaeef960e295c626462
sbx_user1051 <sbx_user1051@169.254.33.29>
2024-02-21 16:57:05 +0000

    Wed Feb 21 16:57:05 UTC 2024

--- test/solution_tests/CHK_R1/test_chk_r1.py
+++ test/solution_tests/CHK_R1/test_chk_r1.py
@@ -9,4 +9,8 @@ class TestCHK_R1():
 
     def test_checkout_offer(self):
         assert checkout_solution.checkout("AAA") == 130
-    
+        assert checkout_solution.checkout("BB") == 45
+        assert checkout_solution.checkout("AAAAAA") == 260
+
+    def test_checkout_offer_and_individual(self):
+        assert checkout_solution.checkout("AAAAAAAA") == 330
