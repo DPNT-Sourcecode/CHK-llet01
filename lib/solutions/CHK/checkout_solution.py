1375a4a1a444f9a10cd7be6ccf4881f2f85ca2db
sbx_user1051 <sbx_user1051@169.254.33.29>
2024-02-21 18:18:31 +0000

    Wed Feb 21 18:18:31 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,7 +12,8 @@ def checkout(skus: str):
             "offers": {
                 3: {
                     "offer": n_for_m_offer,
-                    "price": 130
+                    "price": 130,
+                    "priority": 1  # Doesn't matter atm
                 },
                 5: 200
             },
@@ -20,7 +21,11 @@ def checkout(skus: str):
         'B': {
             "price": 30,
             "offers": {
-                2: 45
+                2: {
+                    "offer": n_for_m_offer,
+                    "price": 45,
+                    "priority": 2
+                }
             }
         },
         'C': {
@@ -35,7 +40,13 @@ def checkout(skus: str):
             "price": 40,
             "offers": {
                 # TODO: Implement this offer
-                2: "get one B free"
+                2: {
+                    "offer": x_for_y_offer,
+                    "price": 0,
+                    "priority": 1,
+                    "offer_sku": "B",
+                    "offer_count": 1
+                }
             }
         }
     }
@@ -50,6 +61,7 @@ def checkout(skus: str):
             items[item] = 1
 
     basket_total = 0
+    offer_queue: dict[int, list] = {}
 
     # Could iterate over the offers first, remove any items from skus that have been applied,
     # then add the individual charge at the end. The offers would be ordered to favor the customer. A offer is no applied
@@ -59,6 +71,7 @@ def checkout(skus: str):
     # Instead, find where all the offers that are available and put them in an ordered list based on priority. Then
     # execute the offers in order until no offers are found. Then add the individual items to the basket total.
 
+    # Find all the offers that are available
     for sku, sku_count in items.items():
         # SKU must be in the prices table
         if sku not in prices:
@@ -67,32 +80,64 @@ def checkout(skus: str):
         # No offers are possible if the count is less than the offer or no offers exist
         # Assuming offers are sorted in ascending order
         if not bool(prices[sku]["offers"]) or sku_count < list(prices[sku]["offers"].keys())[0]:
-            basket_total += sku_count * prices[sku]["price"]
             continue
 
         # Find any 'perfect' offers and add to the basket total
         if sku_count in prices[sku]["offers"].keys():
-            basket_total += prices[sku]["offers"][sku_count]
+            offer_queue[prices[sku]["offers"][sku_count]["priority"]].append(
+                {"sku": sku, "count": sku_count, "offer": prices[sku]["offers"][sku_count]["offer"]})
             continue
 
         # Find the largest offer that is less than the count
-        closest_smaller_offer = 0
+        closest_smaller_offer_count = 0
         closest_smaller_price = 0
         for offer_count, offer_price in prices[sku]["offers"].items():
             if offer_count < sku_count:
-                closest_smaller_offer = offer_count
+                closest_smaller_offer_count = offer_count
                 closest_smaller_price = offer_price
             if offer_count > sku_count:
                 break
 
-        # Add the left over individual items to the basket total
-        basket_total += (sku_count % closest_smaller_offer) * prices[sku]["price"]
-        # Add the offer to the basket total
-        basket_total += (sku_count // closest_smaller_offer) * closest_smaller_price
+        offer_queue[prices[sku]["offers"][closest_smaller_offer_count]["priority"]].append(
+            {"sku": sku, "count": sku_count, "offer": prices[sku]["offers"][closest_smaller_offer_count]["offer"]})
+
+    # Execute the offers in order
+    for priority in sorted(offer_queue.keys()):
+        basket_total += sum(
+            [offer["offer"](items, offer["sku"], offer["count"], offer["offer"]) for offer in offer_queue[priority]])
 
     return basket_total
 
 # for reference
+
+
+# # No offers are possible if the count is less than the offer or no offers exist
+# # Assuming offers are sorted in ascending order
+# if not bool(prices[sku]["offers"]) or sku_count < list(prices[sku]["offers"].keys())[0]:
+#     basket_total += sku_count * prices[sku]["price"]
+#     continue
+
+# # Find any 'perfect' offers and add to the basket total
+# if sku_count in prices[sku]["offers"].keys():
+#     basket_total += prices[sku]["offers"][sku_count]
+#     continue
+#
+# # Find the largest offer that is less than the count
+# closest_smaller_offer = 0
+# closest_smaller_price = 0
+# for offer_count, offer_price in prices[sku]["offers"].items():
+#     if offer_count < sku_count:
+#         closest_smaller_offer = offer_count
+#         closest_smaller_price = offer_price
+#     if offer_count > sku_count:
+#         break
+#
+# # Add the left over individual items to the basket total
+# basket_total += (sku_count % closest_smaller_offer) * prices[sku]["price"]
+# # Add the offer to the basket total
+# basket_total += (sku_count // closest_smaller_offer) * closest_smaller_price
+
+
 # prices = {
 #         'A': {
 #             "price": 50,
@@ -123,4 +168,3 @@ def checkout(skus: str):
 #             }
 #         }
 #     }
-
--- lib/solutions/CHK/offer_helper_functions.py
+++ lib/solutions/CHK/offer_helper_functions.py
@@ -1,7 +1,17 @@
+def n_for_m_offer(items, sku, sku_count, offer):
+    # Check that offer is possible
+    if sku_count < offer.count:
+        return 0
+    value = (sku_count // offer.count) * offer.price
+    items[sku] -= offer.count * (sku_count // offer.count)
+    return value
 
-def n_for_m_offer(sku, sku_count, offer):
-    items_list[sku]
-    return (sku_count // offer.count) * offer.price
 
-
-
+def x_for_y_offer(items, sku, sku_count, offer):
+    # Check if there is enough x for the offer and there is enough y to offer
+    if sku_count < offer.count or items[offer.offer_sku] < offer.offer_count:
+        return 0
+    # Remove the free items from the basket
+    items[offer.offer_sku] -= offer.offer_count
+    # X items will be charged individually at the end
+    return 0
