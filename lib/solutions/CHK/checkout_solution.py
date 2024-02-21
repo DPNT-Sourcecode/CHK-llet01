b88ce05a51f58dd64b8b8bad09bfb6880ea85311
sbx_user1051 <sbx_user1051@169.254.33.29>
2024-02-21 17:06:05 +0000

    Wed Feb 21 17:06:05 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -40,14 +40,24 @@ def checkout(skus: str):
             return -1
 
         # No offers are possible if the count is less than the offer or no offers exist
-        if prices[sku]["offers"] sku_count < prices[sku]["offers"].keys()[0]:
+        # Assuming offers are sorted in ascending order
+        if not bool(prices[sku]["offers"]) or sku_count < prices[sku]["offers"].keys()[0]:
             basket_total += sku_count * prices[sku]["price"]
+            continue
 
-        # Find any offers and add to the basket total
-        if sku_count in prices[sku]["offers"]:
+        # Find any 'perfect' offers and add to the basket total
+        if sku_count in prices[sku]["offers"].keys():
             basket_total += prices[sku]["offers"][sku_count]
             continue
 
+        # Find the largest offer that is less than the count
+        closest_larger_offer = 0
+        for offer_count, offer_price in prices[sku]["offers"].items():
+            if offer_count > sku_count:
+                basket_total += sku_count % offer_count * offer_price
+                
+
 
     return basket_total
+
 
